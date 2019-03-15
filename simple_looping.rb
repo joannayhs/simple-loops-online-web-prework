ead69bfba5736660aba05dabf31555a44cff3cb8
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-15 19:33:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,6 +36,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 0
   while number_of_times <= 7
+  number_of_times += 1
   puts phrase
 end
 end
